bb6bfd3f305bf5e53bc6c43706601ae5238a2067
arjun <arjunshajiforpc@gmail.com>
2023-04-09 12:30:23 +0530

    project completed

--- server/controllers/dashboardContorller.js
+++ server/controllers/dashboardContorller.js
@@ -110,7 +110,7 @@ exports.dashboardUpdateNote = async (req, res) => {
     try {
         await Note.findOneAndUpdate(
             { _id: req.params.id },
-            { title: req.body.title, body: req.body.body }
+            { title: req.body.title, body: req.body.body, updatedAt: Date.now() }
         ).where({ user: req.user.id });
         res.redirect('/dashboard')
     } catch (error) {
@@ -129,6 +129,72 @@ exports.dashboardDeleteNote = async (req, res) => {
             _id: req.params.id
         }).where({ user: req.user.id })
         res.redirect('/dashboard')
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+/**
+ * GET
+ * Add Notes
+ */
+exports.dashboardAddNote = async (req, res) => {
+    res.render('dashboard/add', {
+        layout: '../views/layouts/dashboard'
+    })
+}
+
+/**
+ * POST
+ * Add Notes
+ */
+
+exports.dashboardAddNoteSubmit = async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Note.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+/**
+ * GET
+ * Search
+ */
+
+exports.dashboardSearch = async (req, res) => {
+    try {
+        res.render('dashboard/search', {
+            SearchResult: "",
+            layout: '../views/layouts/dashboard'
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+/**
+ * POST
+ * Search
+ */
+
+exports.dashboardSearchSubmit = async (req, res) => {
+    try {
+        let searchTerm = req.body.searchTerm
+        const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9]/g, '')
+        const searchResults = await Note.find ({
+            $or: [
+              { title: { $regex: searchNoSpecialChars, $options: "i" } },
+              { body: { $regex: searchNoSpecialChars, $options: "i" } }
+            ]
+          }).where({ user: req.user.id })
+        //   console.log(searchResults);        
+        res.render('dashboard/search', {
+            searchResults,
+            layout: '../views/layouts/dashboard'
+        })
     } catch (err) {
         console.log(err);
     }
